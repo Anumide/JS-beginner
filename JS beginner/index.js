89609cb343e2e86cfad1b1d541a08228307a8b68
Alaba Samuel Ayomide <alabasamuelayomide3@gmail.com>
2021-05-20 16:07:11 +0100

    Add files via upload

---
+++ JS beginner/index.js
@@ -0,0 +1,52 @@
+// function to convert fahr to celsius
+const convertFahrToCelsius = (n) => {
+  let toNum = Number(n);
+  if(isNaN(toNum)){
+    return `"${n}" is not a valid number but a/an ${typeof n}`;
+  }
+    return (toNum - 32) * 5/9;
+}
+
+console.log(convertFahrToCelsius('to'));
+
+
+//function for checking YuGiOh
+const checkYuGiOh = (n) => {
+  let arr = [];
+  if(isNaN(n)){
+    return `invalid parameter "${n}"`
+  }
+  for(let i = 1; i <= n; i++){
+    arr.push(i);
+  }
+  console.log(arr);
+  const modArr = arr.map((num) => {
+    switch (true){
+      case (num % 30 == 0):
+        return num = "yu-gi-oh"
+        break;
+      case (num % 15 == 0):
+        return num = "gi-oh"
+        break;
+      case (num % 10 == 0):
+        return num = "yu-oh"
+        break;
+      case (num % 6 == 0):
+        return num = "yu-gi"
+        break;
+      case (num % 5 == 0):
+        return num = "oh"
+        break;
+      case (num % 3 == 0):
+        return num = "gi"
+        break;
+      case (num % 2 == 0):
+        return num = "yu"
+        break;
+      default: return num
+    }
+  });
+    console.log(modArr);
+}
+
+console.log(checkYuGiOh('100'));
